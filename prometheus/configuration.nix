6c23d366f36bf81f33581b08e29799993fcb205f
Akshay Brizmohun <aksh1000@gmail.com>
2025-02-02 19:07:58 +1100

    Add prometheus config

---
+++ prometheus/configuration.nix
@@ -0,0 +1,254 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot = {
+    consoleLogLevel = 0;
+
+    loader = {
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+	consoleMode = "max";
+      };
+    };
+
+    initrd = {
+      verbose = false;
+      # luks.devices."luks-79429c14-7337-4fc2-ac26-4bc7ac4fe0eb".device = "/dev/disk/by-uuid/79429c14-7337-4fc2-ac26-4bc7ac4fe0eb";
+      luks.devices."luks-79429c14-7337-4fc2-ac26-4bc7ac4fe0eb" = {
+        crypttabExtraOpts = [ "fido2-device=auto" ];
+        device = "/dev/disk/by-uuid/79429c14-7337-4fc2-ac26-4bc7ac4fe0eb";
+      };
+      luks.devices."luks-3099b657-9d1b-49f1-956b-38594ed3d01a" = {
+        crypttabExtraOpts = [ "fido2-device=auto" ];
+        device = "/dev/disk/by-uuid/3099b657-9d1b-49f1-956b-38594ed3d01a";
+      };
+      systemd.enable = true;
+      kernelModules = [ "i915" ];
+    };
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+      "i915.modeset=1"
+    ];
+
+    plymouth = {
+      enable = true;
+      theme = "hexagon";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "hexagon" ];
+        })
+      ];
+    };
+  };
+
+  networking.hostName = "prometheus"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Australia/Melbourne";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Disable lid switch
+  services.logind.extraConfig = ''
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+    HandleLidSwitchDocked=ignore
+  '';
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.briz = {
+    isNormalUser = true;
+    description = "briz";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  TODO: Move to home manager
+      ghostty
+      git
+      gum
+      lazygit
+      librewolf
+      google-chrome
+      mpv
+      tmux
+      yazi
+      superfile
+      vscode
+      bat
+      libreoffice-qt
+      hunspell
+      hunspellDicts.en_AU
+      bitwarden-desktop
+      # gparted
+      # sysz
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  programs.neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Yubikey GPG and SSH
+  services.udev.packages = [ pkgs.yubikey-personalization ];
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
+  };
+
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
+
+  # TODO: Harden u2f (https://joinemm.dev/blog/yubikey-nixos-guide#hardening)
+  security.pam.u2f = {
+    enable = true;
+    settings = {
+      interactive = false;
+      cue = false;
+      pinverification = 1;
+      userpresence = 0;
+      userverification = 0;
+    };
+  };
+
+  programs.yubikey-touch-detector = {
+    enable = true;
+    libnotify = true;
+  };
+
+  services.yubikey-agent.enable = true;
+
+  services.udev.extraRules = ''
+      ACTION=="remove",\
+       ENV{ID_BUS}=="usb",\
+       ENV{ID_MODEL_ID}=="0407",\
+       ENV{ID_VENDOR_ID}=="1050",\
+       ENV{ID_VENDOR}=="Yubico",\
+       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+  '';
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  environment.variables = {
+    NIXPKGS_ALLOW_UNFREE = 1;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
